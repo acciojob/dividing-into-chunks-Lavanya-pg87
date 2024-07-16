e20cd8a2e58ba9a5d94f2699905ddb896b469cb2
Lavanya-pg87 <lavanyagangadharan99@gmail.com>
2024-07-16 20:51:19 +0530

    Changes made for the chunck-code.

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
-  // Write your code here
+      const result = [];
+      let currentSubarray = [];
+      let currentSum = 0;
+
+   for(const num of arr){
+    if(currentSum + num <= n){
+      currentSubarray.push(num);
+      currentSum += num;
+    }
+    else{
+      result.push(currentSubarray);
+      currentSum = num;
+    }
+   }
+   if(currentSubarray.length > 0){
+    result.push(currentSubarray);
+   }   
 };
 
 const n = prompt("Enter n: ");
